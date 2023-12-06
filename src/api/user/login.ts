f1fa4f654acf38a7286ca80f3c1ed839cfd4850a
ytwp <54202482+ytwp@users.noreply.github.com>
2023-12-06 10:35:05 +0800

    api

---
+++ src/api/user/login.ts
@@ -0,0 +1,18 @@
+// import { api } from "../base";
+
+export async function loginApi(body: Record<string, any>) {
+  // return api.post('/user/login', body)
+  console.log('loginApi', body)
+  return {
+
+  }
+}
+
+export async function getInfoApi() {
+  // return api.get('/user/getInfo')
+  console.log('getInfoApi')
+  return {
+
+  }
+}
+
