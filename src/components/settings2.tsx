4040effcf3e4bab97d12c432d7b326cc3e2f77fd
ytwp <54202482+ytwp@users.noreply.github.com>
2023-12-13 11:43:01 +0800

    svg icon

--- src/components/settings2.tsx
+++ src/components/settings2.tsx
@@ -1,7 +1,37 @@
+import { Table } from "@douyinfe/semi-ui";
+import Column from "@douyinfe/semi-ui/lib/es/table/Column";
+
 export function Settings() {
+  const backendData = [{
+    id:1,
+    name:'name1'
+  },{
+    id:2,
+    name:'name1'
+  },{
+    id:3,
+    name:'name2'
+  },{
+    id:3,
+    name:'name2'
+  }]
   return (
     <>
-      <div style={{height:'2000px'}}>Settings2</div>
+      <Table dataSource={backendData} pagination={false} >
+          <Column title="id" dataIndex="id" key="id" />
+          <Column title="name" dataIndex="name" key="name"
+            filters={[{
+              text: 'name1',
+              value: 'name1'
+            },{
+              text: 'name2',
+              value: 'name2'
+            }]}
+            onFilter={(value, record) => {
+              return record.name.includes(value)
+            }} />
+        </Table>
+
     </>
   );
 }
